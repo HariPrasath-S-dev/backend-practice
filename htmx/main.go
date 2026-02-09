13cce1f19dc69e1522ed1ba0be20bb8b0244979b
HariPrasath-S-dev <hariprasathsundar3902@gmail.com>
2026-02-10 00:31:08 +0530

    HTMX full code

--- htmx/main.go
+++ htmx/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 type Film struct{
 	Title string
@@ -29,7 +31,27 @@ func main() {
 
 
 	}
+	h2:=func(w http.ResponseWriter, r *http.Request){
+		time.Sleep(1 * time.Second)
+		title:=r.PostFormValue("title")
+		director:=r.PostFormValue("director")
+		
+		tmpl:=template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element",Film{Title: title,Director: director})
+
+		// htmlStr := fmt.Sprintf("<li class=\"list-group-item bg-primary text-white\">%s - %s</li>", title, director)
+		// tmpl,_:=template.New("t").Parse(htmlStr)
+
+		// tmpl.Execute(w,nil)
+
+		// log.Print("HTMX Request")
+
+		// // HX - Request is a header set by HTMX
+		// log.Print(r.Header.Get("HX - Request"))
+	}
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/add-film/",h2)
+
 	log.Fatal(http.ListenAndServe(":8080", nil)) // Specify port number and handler
 
 }
