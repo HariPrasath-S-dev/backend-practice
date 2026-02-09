3a7197b0826c0327aefba0ab5abace2568059fa4
HariPrasath-S-dev <hariprasathsundar3902@gmail.com>
2026-02-10 00:34:41 +0530

    HTMX fix

--- htmx/main.go
+++ htmx/main.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
 )
-type Film struct{
-	Title string
+
+type Film struct {
+	Title    string
 	Director string
 }
+
 func main() {
 	h1 := func(w http.ResponseWriter, r *http.Request) {
 		// io.WriteString(w, "Hello world\n")
 		// io.WriteString(w, r.Method)
 
 		// To read the HTML file
-		tmpl:=template.Must(template.ParseFiles("index.html"))
+		tmpl := template.Must(template.ParseFiles("index.html"))
 
-		films:=map[string][]Film{
-			"Films":{
-			{Title:"Star Wars", Director:"George Lucas"},
-			{Title:"Star Trek", Director:"James Cameron"},
-			{Title: "Avatar", Director:"James Cameron"},
-		},
-	}
+		films := map[string][]Film{
+			"Films": {
+				{Title: "Star Wars", Director: "George Lucas"},
+				{Title: "Star Trek", Director: "James Cameron"},
+				{Title: "Avatar", Director: "James Cameron"},
+			},
+		}
 		// To execute the template
 		tmpl.Execute(w, films)
 
-
 	}
-	h2:=func(w http.ResponseWriter, r *http.Request){
+	h2 := func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(1 * time.Second)
-		title:=r.PostFormValue("title")
-		director:=r.PostFormValue("director")
-		
-		tmpl:=template.Must(template.ParseFiles("index.html"))
-		tmpl.ExecuteTemplate(w, "film-list-element",Film{Title: title,Director: director})
+		title := r.PostFormValue("title")
+		director := r.PostFormValue("director")
+
+		tmpl := template.Must(template.ParseFiles("index.html"))
+		tmpl.ExecuteTemplate(w, "film-list-element", Film{Title: title, Director: director})
 
 		// htmlStr := fmt.Sprintf("<li class=\"list-group-item bg-primary text-white\">%s - %s</li>", title, director)
 		// tmpl,_:=template.New("t").Parse(htmlStr)
@@ -50,7 +50,7 @@ func main() {
 		// log.Print(r.Header.Get("HX - Request"))
 	}
 	http.HandleFunc("/", h1)
-	http.HandleFunc("/add-film/",h2)
+	http.HandleFunc("/add-film/", h2)
 
 	log.Fatal(http.ListenAndServe(":8080", nil)) // Specify port number and handler
 
